1a2dc81305bc4f2224ebd089ca96931cf11c2dff
bernadettestella <bernadettestella27@gmail.com>
2023-06-11 20:37:11 -0700

    matrix corrected

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for y in matrix:
-        print (" ".join("{:d}".format(a) for a in y))
+        print(" ".join("{:d}".format(a) for a in y))
