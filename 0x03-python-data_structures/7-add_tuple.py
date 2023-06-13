12c7a4d50a091b86982651892a0cf75b21909118
bernadettestella <bernadettestella27@gmail.com>
2023-06-12 17:33:53 -0700

    tuples fixed

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def add_tulpe(tulpe_a(), tulpe_b()):
-    a, b = len(tulpe_a), len(tulpe_b)
-    new_tulpe = ((tulpe_a[0] if a >= 1 else 0) + (tulpe_b[0] if b >= 1 else 0),
-                 (tulpe_a[1] if a >= 2 else 0) + (tulpe_b[1] if b >= 2 else 0))
+def add_tuple(tuple_a=(), tuple_b=()):
+    a, b = len(tuple_a), len(tuple_b)
+    new_tulpe = ((tuple_a[0] if a >= 1 else 0) + (tuple_b[0] if b >= 1 else 0),
+                 (tuple_a[1] if a >= 2 else 0) + (tuple_b[1] if b >= 2 else 0))
 
     return new_tulpe
