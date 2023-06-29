1243e2ee5a4eef17d728d1bb02593eb84a688d35
bernadettestella <bernadettestella27@gmail.com>
2023-06-29 11:12:27 -0700

    classes in python

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a simple class square"""
+
+
+class Square:
+    """An empty Square class
+    with no attributes or methods.
+    """
+
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class and initialises it
+"""
+
+
+class Square:
+    """Square implementation
+    """
+
+    def __init__(self, size):
+        self.__size = size
