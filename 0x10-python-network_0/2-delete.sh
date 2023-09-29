c3301b19468f6f57d3f0ff81ab011fb86b27a84b
bernadettestella <bernadettestella27@gmail.com>
2023-09-28 19:22:58 -0700

    HTTP Protocal

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 #sends a DELETE request to the URL passed as the first argument and displays the body of the response
-
-# -s = Silent mode. Don't show progress meter or error messages.
-# X = specifies the request method - DELETE
 curl -sX DELETE "$1"
